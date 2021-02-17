747aa9fd7a11ef5ccaa6da49be26541264e59f9e
Sinem Uyarer <sinemuyarer@gmail.com>
2021-02-17 17:32:09 +0100

    canvas added - templates are no done

--- public/script.js
+++ public/script.js
@@ -1,1 +1,44 @@
 //canvas and signature codes here
+// console.log("sanity check");
+
+const myCanvas = $("#myCanvas");
+const dataURL = myCanvas[0].toDataURL();
+console.log(dataURL);
+const context = myCanvas[0].getContext("2d");
+
+let isMoving = false;
+let x = 0;
+let y = 0;
+
+myCanvas.on("mousedown", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isMoving = true;
+});
+
+myCanvas.on("mousemove", (e) => {
+    if (isMoving === true) {
+        drawMyCanvas(context, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+
+$(window).on("mouseup", (e) => {
+    if (isMoving === true) {
+        drawMyCanvas(context, x, y, e.offsetX, e.offsetY);
+        x = 0;
+        y = 0;
+        isMoving = false;
+    }
+});
+
+function drawMyCanvas(context, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}

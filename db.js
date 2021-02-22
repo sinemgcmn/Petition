dcdb41ee7a3e1d831d6a2cd9b1e4c2ebcdd17885
Sinem Uyarer <sinemuyarer@gmail.com>
2021-02-22 22:09:27 +0100

    changes on deploy

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 const spicedPg = require("spiced-pg");
-const db = spicedPg("postgres:postgres:postgres@localhost:5432/petition");
+
+const db = spicedPg(
+    process.env.DATABASE_URL ||
+        "postgres:postgres:postgres@localhost:5432/petition"
+);
 
 ////////PART 1/////////////////
 
